3f11b7d48fa135d36affdf839e77c5fb5b96ad53
AdamKuzniarski <adam.kuzniarski@gmx.de>
2026-06-09 23:06:36 +0200

    expand imports in app.module.ts with HealthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { HealthModule } from './health/health.module';
+import { appConfig } from './config/app.config';
+import { validate } from './config/env.validation';
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, validate }),
+    ConfigModule.forFeature(appConfig),
+    HealthModule,
+  ],
 })
 export class AppModule {}
