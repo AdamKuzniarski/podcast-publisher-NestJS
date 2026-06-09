eb27ebbf31282460d889038a7dfacc99fb9810f4
AdamKuzniarski <adam.kuzniarski@gmx.de>
2026-06-09 23:07:54 +0200

    bootstrap the dependencies and main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('app.port')!;
+  const corsOrigins = configService.get<string[]>('app.corsOrigins')!;
+
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
-  await app.listen(3000);
+  app.enableCors({ origin: corsOrigins });
+
+  await app.listen(port);
 }
 bootstrap();
